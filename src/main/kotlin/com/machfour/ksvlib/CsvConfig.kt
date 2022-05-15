ca6b2e248e83b6588cb719304082fc552f5ec9e2
Max Fisher <machfour+git@protonmail.com>
2022-05-16 00:17:41 +1000

    Collapse CsvConfig into simple class

--- src/main/kotlin/com/machfour/ksvlib/CsvConfig.kt
+++ src/main/kotlin/com/machfour/ksvlib/CsvConfig.kt
@@ -1,62 +1,29 @@
 package com.machfour.ksvlib
 
-interface CsvConfig {
-    val fieldSeparator: Char
-    val quoteCharacter: Char
-    val useCRLF: Boolean
-    val quoteAllFields: Boolean
-
-    val lineTerminator: String
-        get() = if (useCRLF) "\r\n" else "\n"
+class CsvConfig(
+    val fieldSeparator: Char = DEFAULT_SEPARATOR,
+    val quoteCharacter: Char = DEFAULT_QUOTE_CHAR,
+    val useCRLF: Boolean = false,
+    val quoteAllFields: Boolean = false,
+) {
+    val lineTerminator: String = if (useCRLF) "\r\n" else "\n"
 
     fun copy(
         fieldSeparator: Char? = null,
         quoteCharacter: Char? = null,
         useCRLF: Boolean? = null,
-        quoteAllFields: Boolean? = null,
-    ): CsvConfig
+        quoteAllFields: Boolean? = null
+    ) = CsvConfig(
+            fieldSeparator = fieldSeparator ?: this.fieldSeparator,
+            quoteCharacter = quoteCharacter ?: this.quoteCharacter,
+            useCRLF = useCRLF ?: this.useCRLF,
+            quoteAllFields = quoteAllFields ?: this.quoteAllFields,
+        )
 }
 
 private const val DEFAULT_SEPARATOR = ','
 private const val DEFAULT_QUOTE_CHAR = '"'
 
-val CONFIG_DEFAULT = makeCsvConfig(DEFAULT_SEPARATOR,  DEFAULT_QUOTE_CHAR, useCRLF = false, quoteAllFields = false)
-val CONFIG_WINDOWS = makeCsvConfig(DEFAULT_SEPARATOR, DEFAULT_QUOTE_CHAR, useCRLF = true, quoteAllFields = false)
-val CONFIG_SEMICOLON = makeCsvConfig(';', DEFAULT_QUOTE_CHAR, useCRLF = false, quoteAllFields = false)
-
-fun makeCsvConfig(
-    fieldSeparator: Char = DEFAULT_SEPARATOR,
-    quoteCharacter: Char = DEFAULT_QUOTE_CHAR,
-    useCRLF: Boolean = false,
-    quoteAllFields: Boolean = false,
-): CsvConfig {
-    return CsvConfigImpl(
-        fieldSeparator = fieldSeparator,
-        quoteCharacter = quoteCharacter,
-        useCRLF = useCRLF,
-        quoteAllFields = quoteAllFields,
-    )
-}
-
-private class CsvConfigImpl(
-    override val fieldSeparator: Char,
-    override val quoteCharacter: Char,
-    override val useCRLF: Boolean,
-    override val quoteAllFields: Boolean,
-): CsvConfig {
-
-    override fun copy(
-        fieldSeparator: Char?,
-        quoteCharacter: Char?,
-        useCRLF: Boolean?,
-        quoteAllFields: Boolean?
-    ): CsvConfig {
-        return CsvConfigImpl(
-            fieldSeparator = fieldSeparator ?: this.fieldSeparator,
-            quoteCharacter = quoteCharacter ?: this.quoteCharacter,
-            useCRLF = useCRLF ?: this.useCRLF,
-            quoteAllFields = quoteAllFields ?: this.quoteAllFields,
-
-        )
-    }
-}
+val CONFIG_DEFAULT = CsvConfig(DEFAULT_SEPARATOR,  DEFAULT_QUOTE_CHAR, useCRLF = false, quoteAllFields = false)
+val CONFIG_WINDOWS = CsvConfig(DEFAULT_SEPARATOR, DEFAULT_QUOTE_CHAR, useCRLF = true, quoteAllFields = false)
+val CONFIG_SEMICOLON = CsvConfig(';', DEFAULT_QUOTE_CHAR, useCRLF = false, quoteAllFields = false)
